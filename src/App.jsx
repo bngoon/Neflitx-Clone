d72beab5752089bcd6d5019f5fc33baa477d2276
Booker Ngoon <bookerngoon@gmail.com>
2024-04-29 14:00:28 -0400

    components set up and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Home from './pages//Home/Home.jsx';
 function App() {
   return (
     <div>
-      
+      <Home/>
     </div>
   )
 }
---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './TitleCards.css'
+function TitleCards() {
+  return (
+    <div className='titlecards'>
+      
+    </div>
+  )
+}
+
+export default TitleCards
