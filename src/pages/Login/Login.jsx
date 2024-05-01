bef2e73fa9e9c76bcc19e8824611b2ea77e277ac
Booker Ngoon <bookerngoon@gmail.com>
2024-05-01 12:04:49 -0400

    functions imported to login.jsx. usestate set for name email and password

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import './Login.css';
 import logo from '../../assets/logo.png'
-
+import { login, signup } from '../../firebase';
 
 function Login() {
 
 
 const [signState, setSignState] = useState("Sign In")
+const [name, setName] = useState("")
+const [email, setEmail] = useState("")
+const [password, setPassword] = useState("")
+
+
 
   return (
     <div className='login'>
@@ -14,10 +19,21 @@ const [signState, setSignState] = useState("Sign In")
       <div className="login-form">
         <h1>{signState}</h1>
         <form >
-          {signState==="Sign Up"?<input type="text" placeholder='Your Name'/>: <></>}
+          {signState==="Sign Up"?
+          <input value={name} 
+          onChange={(e)=>{e.target.value}} 
+          type="text" 
+          placeholder='Your Name'/>: <></>}
           
-          <input type="email" placeholder='Email'/>
-          <input type="password" placeholder='Password'/>
+          <input 
+           value={email} 
+           onChange={(e)=>{e.target.value}}
+          type="email" placeholder='Email'/>
+
+          <input 
+           value={password} 
+           onChange={(e)=>{e.target.value}}
+          type="password" placeholder='Password'/>
           <button >Sign Up</button>
           <div className="form-help">
             <div className="remember">
